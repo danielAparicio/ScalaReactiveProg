4df237ba39f53d74bd01c7e3219da44e581fadce
danielAparicio <daniel.aparicio.urbina@gmail.com>
2015-05-08 18:50:35 +0100

    Adding the EclipseAll to create the Eclipse structure

---
+++ sbtEclipseAll.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd actorbintree
+sbt eclipse
+cd ..
+cd nodescala
+sbt eclipse
+cd ..
+cd quickcheck
+sbt eclipse
+cd ..
+cd reactive
+sbt eclipse
+cd ..
+cd simulations
+sbt eclipse
+cd ..
+cd calculator
+sbt eclipse
+cd ..
+cd suggestions
+sbt eclipse
+cd ..
+cd observables_samples_week_4 
+sbt eclipse
+cd ..
+cd actors_testing_week_5
+sbt eclipse
+cd ..
+cd actors_samples_week_5 
+sbt eclipse
+
+
